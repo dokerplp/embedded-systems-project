0f12d142cf55a0c2d000973f048a00b9cd14f875
Valerii Butorin <butorin.v.a@mail.ru>
2022-06-15 21:15:13 +0300

    Delete projects/Spring2022/WALL-E/Java/WALL_E directory

--- projects/Spring2022/WALL-E/Java/WALL_E/src/main/java/Main.java
+++
@@ -1,9 +0,0 @@
-import java.io.*;
-
-public class Main {
-
-    public static void main(String[] args) throws IOException {
-        Server server = new Server(2113);
-        server.start();
-    }
-}
